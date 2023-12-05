8477a492b946646e9dcdd2205bb18fa1865a5c70
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2023-12-05 17:17:09 +0800

    Start on blogs

---
+++ src/BlogList.js
@@ -0,0 +1,23 @@
+const BLOGLIST = [
+  {
+    id: 1,
+    image: "./Images/Blog1.webp",
+    header: "A comfy bedroom",
+    user: "Furvo",
+    data: "5 Dec 2023 ",
+    noOfComments: 2,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, minima? Iste obcaecati corporis, vitae dolorem porro repudiandae voluptatum suscipit voluptate iusto excepturi molestiae cupiditate cumque natus perspiciatis animi dolorum quidem libero velit nihil omnis? Doloribus eligendi quas cum assumenda cupiditate? Accusantium delectus asperiores cum reprehenderit maxime maiores laudantium voluptates sit!",
+    comments: [
+      {
+        user: "furnish",
+        date: "5 Dec 2023",
+        time: "17:12",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, doloribus.",
+      },
+    ],
+  },
+];
+
+export default BLOGLIST;
