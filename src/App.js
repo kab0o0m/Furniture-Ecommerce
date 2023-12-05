e7d1db4a37c79e0c29a2d864b9c40402dce8a05f
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2023-12-05 11:31:51 +0800

    ecommerce website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Nav";
+import Homepage from "./components/Homepage/Homepage";
+import Shop from "./components/Shop/Shop";
+import Blog from "./components/Blog/Blog";
+import AboutUs from "./components/About Us/AboutUs";
+import ContactUs from "./components/Contact Us/ContactUs";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+      </div>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Contact Us/ContactUs.js
@@ -0,0 +1,59 @@
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      {/* Header */}
+      <div className="contact-us-header">
+        <a href="/">Home</a>
+        <div>/</div>
+        <div className="contact-us-header-page">
+          <p>Contact</p>
+        </div>
+      </div>
+      <div className="contact-info">
+        <h1>Contact Info</h1>
+        <div className="contact-details">
+          <div className="contact-details-1">
+            <i class="fa-solid fa-location-dot"></i>
+            <span>456 Comfort Street, Cosyville Singapore 123456</span>
+          </div>
+          <div className="contact-details-1">
+            <i class="fa-solid fa-phone"></i>
+            <span>+65 6978 4212</span>
+          </div>
+          <div className="contact-details-1">
+            <i class="fa-solid fa-envelope"></i>
+            <span>Info@furvo.com</span>
+          </div>
+        </div>
+      </div>
+      <div className="send-message">
+        <h1>Send Message</h1>
+        <form>
+          <div className="form-header">
+            <div className="name">
+              <label htmlFor="name">Your Name</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Your Email</label>
+              <input type="text" id="email" name="email" required />
+            </div>
+          </div>
+          <div className="form-header-2">
+            <label htmlFor="remarks">Message</label>
+          </div>
+          <div className="form-input-2">
+            <textarea name="remarks" id="remarks" rows="10"></textarea>
+          </div>
+          <div className="btn">
+            <button type="submit">Submit Message</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
