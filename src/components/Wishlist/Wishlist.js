cdd30da04c221b3f5ad43bcc2302acfa6ae4b247
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2023-12-07 17:42:42 +0800

    wishlist page updated, can remove from wishlist

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -6,6 +6,19 @@ import { useState } from "react";
 const Wishlist = () => {
   const [wishlistItems, setWishlistItems] = useState(getWishlist);
 
+  const filtered = (item) => {
+    setWishlistItems((wishlistItems) => {
+      let updatedWishlistItems = [...wishlistItems];
+
+      updatedWishlistItems = updatedWishlistItems.filter(
+        (wishlistItem) => wishlistItem.id !== item.id
+      );
+
+      setWishlist(updatedWishlistItems);
+      console.log(updatedWishlistItems);
+      return updatedWishlistItems;
+    });
+  };
   return (
     <div className="wishlist">
       {/* Header */}
@@ -20,17 +33,18 @@ const Wishlist = () => {
       {/* Body */}
       {/* Body */}
       <div className="wishlist-body">
-        {/* Display cart-header */}
+        {/* Display wishlist-header */}
         {wishlistItems.length > 0 && (
           <div className="wishlist-display-header">
             <div className="wishlist-display-img-header">Image</div>
             <div className="wishlist-display-title-header">Product</div>
             <div className="wishlist-display-price-header">Price</div>
             <div className="wishlist-display-discount-header">Discount</div>
+            <div className="wishlist-display-remove-header">Remove</div>
           </div>
         )}
 
-        {/* Display cart-body */}
+        {/* Display wishlist-body */}
         {wishlistItems.length > 0 &&
           wishlistItems.map((item) => (
             <div key={item.id} className="wishlist-display-body">
@@ -40,6 +54,11 @@ const Wishlist = () => {
               <div className="wishlist-display-title">{item.description}</div>
               <div className="wishlist-display-price">SGD {item.price}</div>
               <div className="wishlist-display-discount">None</div>
+              <div className="wishlist-display-remove">
+                <button onClick={() => filtered(item)}>
+                  <i className="fa-solid fa-xmark"></i>
+                </button>
+              </div>
             </div>
           ))}
       </div>
