4a65b81e8721d739b673e83e41e505b068af9f3a
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2023-12-12 16:55:11 +0800

    privacy page and tnc pages done

--- src/components/Privacy/Privacy.js
+++ src/components/Privacy/Privacy.js
@@ -2,8 +2,49 @@ import "./Privacy.css";
 
 const Privacy = () => {
   return (
-    <div>
-      <div></div>
+    <div className="privacy">
+      <h1>Privacy Policy</h1>
+
+      <p>
+        Welcome to Furvo (the "Company," "we," "our," or "us"). This Privacy
+        Policy explains how we collect, use, disclose, and safeguard your
+        information when you visit our website [yourwebsite.com], including any
+        other media form, media channel, mobile website, or mobile application
+        related or connected thereto (collectively, the "Site"). Please read
+        this privacy policy carefully. If you do not agree with the terms of
+        this privacy policy, please do not access the site.
+      </p>
+
+      <p>
+        <strong>Information We Collect</strong>
+      </p>
+
+      <p>
+        We may collect personal information that you voluntarily provide to us
+        when you express an interest in obtaining information about us or our
+        products and services when you participate in activities on the Site, or
+        otherwise when you contact us.
+      </p>
+
+      <p>
+        <strong>How We Use Your Information</strong>
+      </p>
+
+      <p>We use the information we collect or receive:</p>
+
+      <ul>
+        <li>To send you marketing and promotional communications.</li>
+        <li>To deliver targeted advertising to you.</li>
+        <li>For other business purposes.</li>
+      </ul>
+
+      {/* Add more sections as needed */}
+
+      <p>
+        Furvo may update this Privacy Policy from time to time. We will notify
+        you of any changes by posting the new Privacy Policy on the Site. You
+        are advised to review this Privacy Policy periodically for any changes.
+      </p>
     </div>
   );
 };
--- src/components/TNC/TNC.js
+++ src/components/TNC/TNC.js
@@ -2,8 +2,57 @@ import "./TNC.css";
 
 const TNC = () => {
   return (
-    <div>
-      <div></div>
+    <div className="tnc">
+      <h1>Terms and Conditions</h1>
+
+      <p>
+        By accessing this website, you agree to be bound by these terms and
+        conditions of use, all applicable laws and regulations, and agree that
+        you are responsible for compliance with any applicable local laws.
+      </p>
+
+      <p>
+        <strong>Use License</strong>
+      </p>
+
+      <p>
+        Permission is granted to temporarily download one copy of the materials
+        (information or software) on Furvo's website for personal,
+        non-commercial transitory viewing only.
+      </p>
+
+      <p>
+        This is the grant of a license, not a transfer of title, and under this
+        license, you may not:
+      </p>
+
+      <ul>
+        <li>modify or copy the materials;</li>
+        <li>
+          use the materials for any commercial purpose or for any public
+          display;
+        </li>
+        <li>
+          attempt to decompile or reverse engineer any software contained on
+          Furvo's website;
+        </li>
+        <li>
+          remove any copyright or other proprietary notations from the
+          materials;
+        </li>
+        <li>
+          transfer the materials to another person or "mirror" the materials on
+          any other server.
+        </li>
+      </ul>
+
+      {/* Add more sections as needed */}
+
+      <p>
+        If you do not agree with any of these terms, you are prohibited from
+        using or accessing this site. The materials contained in this website
+        are protected by applicable copyright and trademark law.
+      </p>
     </div>
   );
 };
